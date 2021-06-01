33628c6bebff37acda0081e70645f10f991e4cc9
1abhi5hek1shukla <1abhi5hek1shukla@gmail.com>
2021-06-01 21:45:47 +0530

    greyworld support

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ from utils import *
 import time
 
 
-
+handWashingVideo = "./hand_washing_ds/HandWashDataset/HandWashDataset/Step_1/HandWash_001_A_01_G01.mp4"
 
 # Display Windows
 
 cv2.namedWindow("Frame", cv2.WINDOW_NORMAL)
+# cv2.namedWindow("Diff Frame", cv2.WINDOW_NORMAL)
 cv2.namedWindow("Frame_2", cv2.WINDOW_NORMAL)
+
 cv2.resizeWindow('Frame', 640,480)
 cv2.resizeWindow('Frame_2', 640,480)
+# cv2.resizeWindow('Diff Frame', 640,480)
 
 def calcRoi(singleFrame):
 	sh = singleFrame.shape
@@ -18,7 +21,7 @@ def calcRoi(singleFrame):
 	return [cy//2, 3*cy//2, cx//2, 3*cx//2] 
 
 
-cap = cv2.VideoCapture("./hand_washing_ds/HandWashDataset/HandWashDataset/Step_1/HandWash_012_A_01_G03.mp4")
+cap = cv2.VideoCapture(handWashingVideo)
 
 
 # ret, frame = cap.read()
@@ -29,24 +32,35 @@ cap = cv2.VideoCapture("./hand_washing_ds/HandWashDataset/HandWashDataset/Step_1
 # frame = cv2.imread("./sampleImage.jpg")
 # frame = greyWorld(frame)
 
+mFrame = get_median_Frame(handWashingVideo)
+
+kernel = np.ones((3,3),np.uint8)
 
 while True:
     ret, frame = cap.read()
     if not ret:
         break
     
+    # diffFrame = get_median_abs_diff(frame, mFrame)
+    
+    frame = greyWorld(frame)
     
     cv2.imshow("Frame", frame)
-    t,b,l,r = calcRoi(frame)
+    # cv2.imshow("Diff Frame", diffFrame)
+    # t,b,l,r = calcRoi(frame)
+    
     f, i = getPixMap(frame)
+    
     f = np.uint8(255*f)
     
     ret, thresh = cv2.threshold(f,t,255, cv2.THRESH_BINARY)
+
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
     
-    segment = np.zeros(thresh.shape, dtype=thresh.dtype)
-    segment[t:b,l:r] =thresh[t:b,l:r]
-    
-    cv2.imshow("Frame_2", segment)
+    # segment = np.zeros(thresh.shape, dtype=thresh.dtype)
+    # segment[t:b,l:r] =thresh[t:b,l:r]
+
+    cv2.imshow("Frame_2", opening)
     
     k = cv2.waitKey(120) & 0xff
     
--- utils.py
+++ utils.py
@@ -1,7 +1,13 @@
 import cv2
+
 import numpy as np
+np.seterr(divide='ignore', invalid='ignore')
+
 import matplotlib.pyplot as plt
 
+
+
+# Functions ####################################################
 def displayPlt(image, cmap=None):
     if cmap:
         plt.imshow(image, cmap)
@@ -109,7 +115,58 @@ def on_change_thresh(value):
 
 
 
+def get_median_Frame(path):
+    video_stream = cv2.VideoCapture(path)
 
+
+    # # Randomly select 30 frames
+    frameIds = video_stream.get(cv2.CAP_PROP_FRAME_COUNT) * np.random.uniform(size=30)
+
+    # # Store selected frames in an array
+    frames = []
+    for fid in frameIds:
+        video_stream.set(cv2.CAP_PROP_POS_FRAMES, fid)
+        ret, frame = video_stream.read()
+        frames.append(frame)
+        
+    video_stream.release()
+
+    medianFrame = np.median(frames, axis=0).astype(dtype=np.uint8)
+
+    avgFrame = np.average(frames, axis=0).astype(dtype=np.uint8)
+
+    grayMedianFrame = cv2.cvtColor(medianFrame, cv2.COLOR_BGR2GRAY)
+
+
+    return grayMedianFrame
+
+
+def get_median_abs_diff(rawFrame, medFrame, blurring = False, threshing = True):
+    gframe = cv2.cvtColor(rawFrame, cv2.COLOR_BGR2GRAY)
+
+    dframe = cv2.absdiff(gframe, medFrame)
+    
+    if blurring:
+        dframe = cv2.GaussianBlur(dframe, (21, 21), 0)  
+
+    if threshing:
+        ret, dframe = cv2.threshold(dframe,254,255,
+                               cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    return dframe
+
+def biggestContour(contours):
+    biggest = np.array([])
+    max_area = 0
+    for i in contours:
+        area = cv2.contourArea(i)
+        if area > 50:
+            peri = cv2.arcLength(i, True)
+            # approx = cv2.approxPolyDP(i, 0.02 * peri, True)
+            # if area > max_area and len(approx) == 4:
+            if area > max_area:
+                # biggest = approx
+                max_area = area
+    return biggest,max_area
 
 
 # RElated to Vidoe Runn
